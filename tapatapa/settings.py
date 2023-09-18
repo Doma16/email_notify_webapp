cf2621e39ce3347410134ec43e323c2f2f405b58
domaSaric <ds53234@fer.hr>
2023-09-18 21:56:07 +0200

    made config and put in gitignore

--- tapatapa/settings.py
+++ tapatapa/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import json
+
+with open('config.json','r') as f:
+    conf = json.load(f)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -121,6 +126,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
@@ -135,6 +141,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'dominiksaric0501@gmail.com'
-EMAIL_HOST_PASSWORD = 'rxghzkjjpyxejipu'
+EMAIL_HOST_USER = conf['EMAIL_HOST_USER']
+EMAIL_HOST_PASSWORD = conf['EMAIL_HOST_PASSWORD']
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
